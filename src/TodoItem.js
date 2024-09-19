7de8723696faf4ce9342ac4962e8bb561f7df62a
Alirio Isea <isealirio@gmail.com>
2024-09-19 19:36:09 -0400

    style: ajustando padding del main en responsive design

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,8 +3,18 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="todo-item">
-      <span className="Icon Icon-check Icon-check--active">V</span>
-      <p className="todo-item-text todo-item-text--completed">{props.text}</p>
+      <span
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+      >
+        V
+      </span>
+      <p
+        className={`todo-item-text ${
+          props.completed && 'todo-item-text--completed'
+        }`}
+      >
+        {props.text}
+      </p>
       <span className="Icon Icon-delete">X</span>
     </li>
   );
