26f0a788a8bafa0a77b7623f69f38258083bae2f
Alirio Isea <isealirio@gmail.com>
2024-09-20 19:03:36 -0400

    feat: filtrar TODOs con TodoSearch

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ function App() {
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
   return (
     <>
       <h1>TODO Machine</h1>
@@ -39,7 +45,7 @@ function App() {
           />
 
           <TodoList>
-            {defaultTodos.map((todo) => (
+            {searchedTodos.map((todo) => (
               <TodoItem
                 key={todo.text}
                 text={todo.text}
